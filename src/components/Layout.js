06326ed254b2584718cf5dba7f9ec2e637c623b7
0xseo <87014012+0xseo@users.noreply.github.com>
2023-10-30 18:25:37 +0900

    sidebar frame

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,44 @@
 import { Outlet } from "react-router-dom";
-import styles from '../css/header.module.css';
+import { useResizeSidebar } from "../hooks/useResizeSidebar";
+import styles from '../css/layout.module.css';
 
 const Layout = () => {
+  const sidebarInitialSize = 300;
+  const sidebarMinWidth = 100;
+  const sidebarMaxWidth = 500;
+  const { resizing, size, startResizing, stopResizing, updateSize } = 
+    useResizeSidebar(sidebarInitialSize, sidebarMinWidth, sidebarMaxWidth)
   return (
-    <div>
-      <header className={styles.name}>Header</header>
-      <main>
-        <Outlet />
-      </main>
+    <div className={resizing ? styles.containerResizing : styles.container} 
+      onPointerMove={updateSize} onPointerUp={stopResizing}
+    >
+      <div className={styles.sidebarWrapper} style={{flexBasis:size}}>
+        <div className={styles.sidebarContent}>
+          <h2>Sidebar</h2>
+          <ul>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+          </ul>
+        </div>
+        <div 
+          onPointerDown={startResizing}
+          className={styles.sidebarBorder}>
+        </div>
+      </div>
+      <div className={styles.mainWrapper}>
+        <header className={styles.headerWrapper}><h1>Header</h1></header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };

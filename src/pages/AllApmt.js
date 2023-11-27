632713d33d2a6cdfcd6576a3bd7fda4b64f6cdf4
claire <kjeiun@yonsei.ac.kr>
2023-11-27 22:56:16 +0900

    즐겨찾기랑 내약속 컨테이너 ui까지 완료

--- src/pages/AllApmt.js
+++ src/pages/AllApmt.js
@@ -5,8 +5,16 @@ import { IoSyncOutline } from "react-icons/io5";
 import { AiOutlineFileAdd } from "react-icons/ai";
 import { AiFillStar } from "react-icons/ai";
 import { RiSearchLine } from "react-icons/ri";
+import { svgList } from 'assets/svg';
+import {useState, useEffect, useRef} from 'react';
 
 const AllApmt = () => {
+
+  const [favoritesDownClick, setFavoritesDownClick] = useState(false);
+
+  const handleFavoritesDownClick = () =>{
+    setFavoritesDownClick(true);
+  }
   return <div>
     전체 약속 목록
     {/* <ApmtBlock /> */}
@@ -17,8 +25,30 @@ const AllApmt = () => {
         <button className={styles.syncApmt}>{<IoSyncOutline size={24} />}<div className={styles.btnText}>비회원으로 참여한 약속 불러오기</div></button>
       </div>
       <div className={styles.searchContent}>{<RiSearchLine size="18px" color='#888' className={styles.icon}></RiSearchLine>}<input className={styles.searchContentInput} placeholder='찾기'></input></div>
-      <div className={styles.favoritesContainer}>
-        <div className={styles.folderitem}><div>아이템</div></div>
+      <div className={styles.folderContainer}>
+        <div className={styles.folderHeader}>즐겨찾기<div className={styles.icon}>{svgList.folder.favoritesDown}</div></div>
+        <div className={styles.folderInnerContainer}>
+          <div className={styles.folderBox}>
+            <div className={styles.folderIcon}>{svgList.folder.Apmt}</div>
+            <div className={styles.folderName}>
+              <div className={styles.icon}>{svgList.folder.star}</div>
+              합주계획
+              </div>
+          </div>
+          <div className={styles.folderBox}>
+            <div className={styles.folderIcon}>{svgList.folder.Apmt}</div>
+            <div className={styles.folderName}>
+              <div className={styles.icon}>{svgList.folder.star}</div>
+              안녕하세요폴더이름이 길면 어떻게 될까요
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.folderContainer}>
+        <div className={styles.folderHeader}>내 약속</div>
+        <div className={styles.innerContainer}>
+          
+        </div>
       </div>
     </div>
     </div>
